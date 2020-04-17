23ba6fdca6ced54cdb34affded4bf024c2af913c
kishanl25 <63793661+kishanl25@users.noreply.github.com>
2020-04-17 15:52:12 +0530

    Updated LibraryController

--- demonew/demonew/src/main/java/com/kishan/example/controller/LibraryController.java
+++ demonew/demonew/src/main/java/com/kishan/example/controller/LibraryController.java
@@ -1,5 +1,6 @@
 package com.kishan.example.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kishan.example.entity.Book;
 import com.kishan.example.entity.Library;
+import com.kishan.example.model.LibraryModel;
 import com.kishan.example.service.BookService;
 import com.kishan.example.service.LibraryService;
 
@@ -26,14 +29,18 @@ public class LibraryController {
 	@Autowired
 	BookService bookService;
 	
-	@GetMapping(path = "/libraries", produces = "application/json")
+	@GetMapping(path = "/libraries")
     @ResponseBody
-	public Iterable<Library> getLibraries(){
+	public List<LibraryModel> getLibraries(){
+		List<LibraryModel> libraryNameList = new ArrayList<LibraryModel>();
 		Iterable<Library> libraryList = libraryService.getAllLibraries();
 		for (Library lib : libraryList) {
+			LibraryModel libraryModel = new LibraryModel();
 			System.out.println(lib.getLibraryName());
+			libraryModel.setLibraryName(lib.getLibraryName());
+			libraryNameList.add(libraryModel);
 		}
-		return libraryList;
+		return libraryNameList;
 	}
 	
 	@GetMapping(path = "/books", produces = "application/json")
@@ -46,10 +53,11 @@ public class LibraryController {
 		return bookList;
 	}	
 	 
-	 @PostMapping(path= "/addLibrary", consumes = "application/json", produces = "application/json")
-	    public void addLibrary(@RequestBody Library library) 
+	 @RequestMapping(value= "/addLibrary", method=RequestMethod.POST)
+	    public String addLibrary(@RequestBody Library library) 
 	    {
 	        libraryService.saveLibrary(library);
+			return "Successful";
 	         
 	        	         
 	    }
